6de77e169dbf8135882502aebaf74e0bfd0f8fa8
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2024-06-28 09:10:16 -0300

    Reevio da atividade IsPalindrome e numerosMaxEmin
    mudanças feitas com base no comentario.

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-function strConversao(str) {
-    return str.replace(/( )+/g, "");
-}
-
 function isPalindrome(str) {
-    //conversões
-    let strMaiuscula = str.toUpperCase();
-    let strSemEspaco = strConversao(strMaiuscula);
-    let strFeita = strSemEspaco.split("");
+    let strFeita = str.toUpperCase();
+    strFeita = strFeita.replaceAll(" ", "");
+    strFeita = strFeita.split("");
 
-    //verificações
     let indiceNegativo = 0;
     for(let indice = 0; indice<strFeita.length; indice++){
         indiceNegativo++
@@ -20,20 +14,14 @@ function isPalindrome(str) {
 }
 
 function arrayMaxMin(arr){
-    var numerosMaxEMin = [0, 0];
-
-    //verificação máxima
-    var numerosMax = [0];
+    let numerosMaxEMin = [0, 0];
     for (let indice = 0; indice<arr.length; indice++) {
-        if(numerosMax[0] <= arr[indice]) {
-            numerosMax[0] = arr[indice]
+        if(numerosMaxEMin[1] <= arr[indice]) {
+            numerosMaxEMin[1] = arr[indice]
         }
     }
-    numerosMaxEMin[1] = numerosMax[0];
-
-    //verificação mínima
-    var numerosMin = [0];
-    numerosMin[0] = numerosMax[0]
+    let numerosMin = [0];
+    numerosMin[0] = numerosMaxEMin[1]
     for (let indice = 0; indice<arr.length; indice++) {
         if(numerosMin[0] > arr[indice]) {
             numerosMin[0] = arr[indice]

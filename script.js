5ceb210ed8f49689acb507d8c0bf3a9fcd68443a
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2024-07-04 12:02:12 -0300

    atualização padronização ponto e virgula
    coloquei os pontos e virgulas, que achei necessário.

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ function isPalindrome(str) {
     for(let indice = 0; indice<strFeita.length; indice++){
         indiceNegativo++
         if(strFeita[indice] != strFeita[strFeita.length -indiceNegativo]){
-            return false
+            return false;
         }
     }
-    return true
+    return true;
 }
 
 function arrayMaxMin(arr){
     let numerosMaxEMin = [0, 0];
     for (let indice = 0; indice<arr.length; indice++) {
         if(numerosMaxEMin[1] <= arr[indice]) {
-            numerosMaxEMin[1] = arr[indice]
+            numerosMaxEMin[1] = arr[indice];
         }
     }
     let numerosMin = [0];
     numerosMin[0] = numerosMaxEMin[1]
     for (let indice = 0; indice<arr.length; indice++) {
         if(numerosMin[0] > arr[indice]) {
-            numerosMin[0] = arr[indice]
+            numerosMin[0] = arr[indice];
         }
     }
     numerosMaxEMin[0] = numerosMin[0];
-    return numerosMaxEMin
+    return numerosMaxEMin;
 }
